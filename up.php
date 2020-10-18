817bedb380da666d6d876057416c8e19d28e0125
qiuzhuowei <53961021+qiuzhuowei@users.noreply.github.com>
2020-10-18 15:23:04 +0800

    Update up.php

--- up.php
+++ up.php
@@ -16,7 +16,7 @@ date_default_timezone_set("PRC");
 define("REPO","repo");//必须是下面用户名下的公开仓库
 define("USER","username");//必须是当前GitHub用户名
 define("MAIL","mail@foxmail.com");//
-define("TOKEN","token");//https://github.com/settings/tokens 去这个页面生成一个有写权限的token（write:packages前打勾）
+define("TOKEN","token");//https://github.com/settings/tokens 这个页面生成一个有写权限的token（write:packages前打勾）
 
 function upload($url, $content)
 {
